e9d484eb91d743a7e5565c4fa928deab66d2f502
Shaan <Shaan@admins-MacBook-Pro.local>
2020-02-13 14:19:02 -0800

    calculates tip

--- calculate.js
+++ calculate.js
@@ -1,13 +1,26 @@
-const inputBill = document.getElementById('input-bill');
-console.log(inputBill.value) //shows the value of input
-inputBill.addEventListener('change', function(e) {
-    console.log(inputBill.value) 
-})
-const inputTip = document.getElementById('input-tip');
-inputTip.addEventListener('change', function(e) {
-    console.log(inputTip.value)
-})
-const inputPeople = document.getElementById('input-people');
-inputPeople.addEventListener('change', function(e){
-    console.log(inputPeople.value)
-})
+const inputBill = document.getElementById('input-bill')
+const inputTip = document.getElementById('input-tip')
+const inputPeople = document.getElementById('input-people')
+
+const displayTip = document.getElementById('display-tip')
+const displayTotal = document.getElementById('display-total')
+const displayPeople = document.getElementById('display-people')
+
+inputBill.addEventListener('change', handleBill)
+inputTip.addEventListener('change', handleBill)
+inputPeople.addEventListener('change', handleBill)
+
+
+function handleBill() {
+    const bill = parseInt(inputBill.value)
+    const percent = parseInt(inputTip.value)
+    const people = parseInt(inputPeople.value)
+
+    const tip = bill * percent / 100
+    const total = bill + tip
+    const per_person = total / people 
+
+    displayTip.innerHTML = '$' + tip.toFixed(2)
+    displayTotal.innerHTML = '$' + total.toFixed(2)
+    displayPeople.innerHTML= '$' + per_person.toFixed(2)
+}
